55f539146278d32eca722d9b8b6dd548e8915502
afahmy519@gmail.com <afahmy519@gmail.com>
2021-04-17 16:00:14 -0400

    changed color opacity when button clicked

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -21,8 +21,15 @@ class ViewController: UIViewController {
         
         
         playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              //Bring's sender's opacity back up to fully opaque.
+              sender.alpha = 1.0
+          }
     }
-    
+    // Used stack overflow for following code, made minor edits
     func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: "\(soundName)", withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
